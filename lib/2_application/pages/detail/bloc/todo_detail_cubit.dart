1c83be9551a1263cd1c542919eb70db75e13251d
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-01-30 19:58:59 +0100

    Add ToDoDetailPage, its bloc & states views, refactor routes

---
+++ lib/2_application/pages/detail/bloc/todo_detail_cubit.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/1_domain/entities/unique_id.dart';
+import 'package:todo_app/1_domain/use_cases/load_todo_entry_ids_for_collection.dart';
+import 'package:todo_app/core/use_case.dart';
+
+part 'todo_detail_state.dart';
+
+class ToDoDetailCubit extends Cubit<ToDoDetailState> {
+  ToDoDetailCubit({
+    required this.collectionId,
+    required this.loadToDoEntryIdsForCollection,
+  }) : super(ToDoDetailLoadingState());
+
+  final CollectionId collectionId;
+  final LoadToDoEntryIdsForCollection loadToDoEntryIdsForCollection;
+
+  Future<void> fetch() async {
+    emit(ToDoDetailLoadingState());
+    try {
+      final entryIds = await loadToDoEntryIdsForCollection.call(
+        CollectionIdParam(collectionId: collectionId),
+      );
+
+      if (entryIds.isLeft) {
+        emit(ToDoDetailErrorState());
+      } else {
+        emit(ToDoDetailLoadedState(entryIds: entryIds.right));
+      }
+    } on Exception {
+      emit(ToDoDetailErrorState());
+    }
+  }
+}
