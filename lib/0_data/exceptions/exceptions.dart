bd813abd4605c0c624ee9e02e993cc70c968ee47
Magdalena Lonc <magdalena.lonc@gmail.com>
2024-02-14 15:56:10 +0100

    Add exception for server & implement collection methods

--- lib/0_data/exceptions/exceptions.dart
+++ lib/0_data/exceptions/exceptions.dart
@@ -1,3 +1,11 @@
+class ServerException implements Exception {}
+
+class FirestoreCollectionNotFoundException implements ServerException {
+  FirestoreCollectionNotFoundException({required this.id});
+  
+  final String id;
+}
+
 class CacheException implements Exception {}
 
 class CollectionNotFoundException implements CacheException {}
